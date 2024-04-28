75454aa5eacc81b120a65214e61f589e2cb7b1e9
daektumund <134651782+daektumund@users.noreply.github.com>
2024-04-29 10:36:57 +1200

    Has keylistener, but doesn't currently work.

--- src/Canvas.java
+++ src/Canvas.java
@@ -1,7 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.util.ArrayList;
-public class Canvas extends JFrame {
+public class Canvas extends JFrame implements KeyListener {
+    int playerX = 5;
+    int playerY= 5;
+    int playerLevel;
     public Canvas(){
         this.setTitle("Poot Poot");
         this.getContentPane().setPreferredSize(new Dimension(1000,980));
@@ -14,6 +19,28 @@ public class Canvas extends JFrame {
     public void paint(Graphics g){
         super.paint(g);
         Graphics2D g2 = (Graphics2D) g;
-        g2.drawRect(50,50,50,50);
+        g2.drawRect(playerX,playerY,50,50);
+    }
+    public void keyPressed(KeyEvent e){
+        switch (e.getKeyCode()){
+            case (KeyEvent.VK_W):
+                playerY -= 5;
+                break;
+            case (KeyEvent.VK_A):
+                playerX -= 5;
+                break;
+            case (KeyEvent.VK_S):
+                playerY += 5;
+                break;
+            case (KeyEvent.VK_D):
+                playerX += 5;
+                break;
+        }
+    }
+    public void keyReleased(KeyEvent e){
+        System.out.println("Released");
+    }
+    public void keyTyped(KeyEvent e){
+        System.out.println("Typed");
     }
 }
